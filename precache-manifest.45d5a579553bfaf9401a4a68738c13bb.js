3fdae0f678732d1ad462b8991feca4f0206a74ac
pradeep121212 <pradeepkumaranr@gmail.com>
2020-09-24 18:17:16 +0530

    Updates

---
+++ precache-manifest.45d5a579553bfaf9401a4a68738c13bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6038b3183de9b3a1775dc914826cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50e85b98e5d9db1a79ad",
+    "url": "/static/css/main.91c69624.chunk.css"
+  },
+  {
+    "revision": "ac0dd437f71732fb88f0",
+    "url": "/static/js/2.a2d61a50.chunk.js"
+  },
+  {
+    "revision": "bc3d92b8a267d342eb7718b9a67f62b8",
+    "url": "/static/js/2.a2d61a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e85b98e5d9db1a79ad",
+    "url": "/static/js/main.261bb79a.chunk.js"
+  },
+  {
+    "revision": "a5598eed53578b000416",
+    "url": "/static/js/runtime-main.17b14906.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
